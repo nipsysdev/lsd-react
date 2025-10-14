1b8a7535131477f951816821cb83005d94936cc0
Xavier Saliniere <bonjour@xaviers.sh>
2025-10-14 11:09:46 -0400

    fix(dialog,select): minor style improvements

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -67,7 +67,7 @@ function DialogContent({
         {showCloseButton && (
           <DialogPrimitive.Close
             data-slot="dialog-close"
-            className="focus:ring-2 focus:ring-offset-2 focus:outline-hidden data-[state=open]:bg-lsd-surface-secondary data-[state=open]:text-lsd-text absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
+            className="p-2 focus:outline-hidden data-[state=open]:bg-lsd-surface-secondary data-[state=open]:text-lsd-text absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4 cursor-pointer"
           >
             <XIcon />
             <span className="sr-only">Close</span>
