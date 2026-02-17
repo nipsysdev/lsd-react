f9d1f2aff9a83f895a8cbcb730f627536f79edbc
Xav <bonjour@xaviers.sh>
2026-02-16 22:58:54 -0500

    fix: add missing menubar export

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ export * from '@/components/ui/command';
 export * from '@/components/ui/dialog';
 export * from '@/components/ui/input';
 export * from '@/components/ui/label';
+export * from '@/components/ui/menubar';
 export * from '@/components/ui/popover';
 export * from '@/components/ui/progress';
 export * from '@/components/ui/select';
